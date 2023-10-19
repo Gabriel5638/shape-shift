ead7e93a08ea4e30b3ed58dbc998a771bab230c9
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-10-19 14:29:52 +0000

    Finish paleo keto carnivore and whole diet pages by creating html views and urls and adding necessary video and styling to the pages

--- diet/views.py
+++ diet/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def mediterranean_diet(request):
     return render(request, 'diet/mediterranean.html')
+
+def paleo_diet(request):
+    return render(request, 'diet/paleo.html')
+
+def keto_diet(request):
+    return render(request, 'diet/keto.html')
+
+def carnivore_diet(request):
+    return render(request, 'diet/carnivore.html')
+
+def whole_diet(request):
+    return render(request, 'diet/wholediet.html')
