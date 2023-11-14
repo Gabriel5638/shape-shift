6dea6456772afb16b3b1b3558731a48df6f7de10
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-11-14 19:11:08 +0000

    Add signout page and js script for messege timeout after the user logs out

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.contrib.auth import logout
 # Create your views here.
+
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have successfully logged out.')
+    return redirect('home')
