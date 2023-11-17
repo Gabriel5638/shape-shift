4c7c24615e49d1835bf711f4912d6230a5e3aefb
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-11-17 19:08:55 +0000

    Add the front end for the newsletter subscription using bootstrap and form method post in html

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth import logout
+from django.conf import settings
+import mailchimp_marketing as MailchimpMarketing
+from mailchimp_marketing.api_client import ApiClientError
+
 # Create your views here.
 
 def index(request):
@@ -12,3 +16,5 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'You have successfully logged out.')
     return redirect('home')
+
+
