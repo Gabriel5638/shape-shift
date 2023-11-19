26e03fe2ca77503f8341d56f366c4db71307212d
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-11-19 17:49:19 +0000

    Change the utilis.py to fit the cart item model

--- store/utils.py
+++ store/utils.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
 
 def calculate_cart_total(cart_items):
-    total = sum(item['price'] for item in cart_items)
+    total = sum(item.price for item in cart_items)
     return total
