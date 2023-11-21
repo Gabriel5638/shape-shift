c2ae61c37db91c5a0cacaa3b5f47d113e05e6959
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-11-21 00:27:22 +0000

    re try to deploy to heroku by changing settings

--- shape_shift/settings.py
+++ shape_shift/settings.py
@@ -119,16 +119,19 @@ WSGI_APPLICATION = 'shape_shift.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-# If using an environment variable for database configuration (e.g., for Heroku)
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
