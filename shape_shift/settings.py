ffece9b5cfff419132ea2fb75182f33d5e8f6d22
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-11-21 16:35:55 +0000

    Add shapeshift heroku to allowed hosts

--- shape_shift/settings.py
+++ shape_shift/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shapeshift.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['shapeshift-b371800ba4bb.herokuapp.com', 'localhost']
 
 
 
