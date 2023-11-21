72acfe638c1fe677c64d350c78f9de3356c97e27
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-11-21 16:32:33 +0000

    change redirect

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.conf import settings
