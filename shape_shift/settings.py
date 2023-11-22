09fd36178653633a6557c2d8da4d630072a3839f
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-11-22 16:30:16 +0000

    add email authentication to settings

--- shape_shift/settings.py
+++ shape_shift/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR =  os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shapeshift-b371800ba4bb.herokuapp.com', 'localhost', '8000-gabriel5638-shapeshift-wyp77opfy3r.ws-eu106.gitpod.io']
 
@@ -104,8 +104,6 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -203,3 +201,16 @@ CSP_DEFAULT_SRC = ("'self'",)
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
 
 CSRF_TRUSTED_ORIGINS = ['shapeshift-b371800ba4bb.herokuapp.com', 'localhost']
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'shapeshift@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
