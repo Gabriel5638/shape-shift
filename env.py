7cc8ced6dc78dbf80a8268a8015867cdd346b718
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-12-06 14:42:32 +0000

    Create new database in elephant sql

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
 import os
 
-os.environ["SECRET_KEY"] = "@bqgs#l5jx10o*&rlrdz8w@#2u6zs3z9g0t)!xlqf%$h5s4%%$"
+os.environ["DATABASE_URL"] = "postgres://wcukslup:ec7NiAdZLupDF92uev6n1f5zho8w5SGM@tai.db.elephantsql.com/wcukslup"
+os.environ["SECRET_KEY"] = "06851123fe964fc563dc8533c299ec87958a7b28859c1139354caa28664cc08b"
 os.environ["CLOUDINARY_CLOUD_NAME"] = "dgqbwyrsz"
 os.environ["CLOUDINARY_API_KEY"] = "851214297734622"
 os.environ["CLOUDINARY_API_SECRET"] = "r1GWkk9a6ie3NhtGAHDGmeCU0Mo"
