bacdba95a2de50dbd36b7e3695be8c6f713f0839
Gabriel Puiu <Gabrielpuiu213@gmail.com>
2023-12-08 23:43:25 +0000

    Add secret api in env.py and refrence them in settings.py

--- env.py
+++ env.py
@@ -10,3 +10,5 @@ os.environ["EMAILJS_API_KEY"] = "u7WvWFLqomhXd-j1w"
 os.environ["EMAILJS_SERVICE_ID"] = "service_te8a228"
 os.environ["EMAILJS_TEMPLATE_ID"] = "template_7bdyfvy"
 os.environ["EMAIL_HOST_PASSWORD"] = "fwpphmegwtenhxko"
+os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51OFMSqAn3CDk8XfLke7zXtjnbxcg1C89ZS8oNkausybu1YDziS5QcedaQijLFBGSVJ8zZOZNNwzOjBnxvSrTf4qa00ZHVitcze'
+os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51OFMSqAn3CDk8XfLu3btcPvPuDYGfj7Wiz5gdY40cH2iMrJyN8c4ShhE7QyRmLGKjly4OC0abC6KuJeb3OpgYaRY00KCDrMVFq'
